5c31c2bb1fbe80c2fb3898b730d22a1b6b4b7c28
Kyle Chung <kylechun9@gmail.com>
2024-02-19 08:57:47 +0800

    feat: add otlp grpc exporter option

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
 """Application code to test the library-level telemetry."""
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
-
-from opentelemetry import metrics
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader, ConsoleMetricExporter
-
 from fake_evaluator.foo import do_something
+from opentelemetry import metrics, trace
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import \
+    OTLPMetricExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import \
+    OTLPSpanExporter
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import (ConsoleMetricExporter,
+                                              PeriodicExportingMetricReader)
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (BatchSpanProcessor,
+                                            ConsoleSpanExporter)
 
+COLLECTOR_ENDPOINT = "http://127.0.0.1:4317"  # do not use https for all-in-one local test
+if COLLECTOR_ENDPOINT:
+    processor = BatchSpanProcessor(OTLPSpanExporter(endpoint=COLLECTOR_ENDPOINT))
+    reader = PeriodicExportingMetricReader(
+        OTLPMetricExporter(endpoint=COLLECTOR_ENDPOINT)
+    )
+else:
+    processor = BatchSpanProcessor(ConsoleSpanExporter())
+    reader = PeriodicExportingMetricReader(ConsoleMetricExporter())
 
 resource = Resource(attributes={
     SERVICE_NAME: "guardrail-service"
 })
-
 traceProvider = TracerProvider(resource=resource)
-processor = BatchSpanProcessor(ConsoleSpanExporter())
 traceProvider.add_span_processor(processor)
 trace.set_tracer_provider(traceProvider)
-
-reader = PeriodicExportingMetricReader(ConsoleMetricExporter())
 meterProvider = MeterProvider(resource=resource, metric_readers=[reader])
 metrics.set_meter_provider(meterProvider)
 
