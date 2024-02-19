c53489e615833d59c94e9ff3b8c7ed6d844dddf0
Kyle Chung <kylechun9@gmail.com>
2024-02-19 20:01:40 +0800

    chore: use black for isort

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 """Application code to test the library-level telemetry."""
 from fake_evaluator.foo import do_something
 from opentelemetry import metrics, trace
-from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import \
-    OTLPMetricExporter
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import \
-    OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import (ConsoleMetricExporter,
-                                              PeriodicExportingMetricReader)
+from opentelemetry.sdk.metrics.export import (
+    ConsoleMetricExporter,
+    PeriodicExportingMetricReader,
+)
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (BatchSpanProcessor,
-                                            ConsoleSpanExporter)
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
 
 COLLECTOR_ENDPOINT = "http://localhost:4317"  # do not use https for all-in-one local test
 if COLLECTOR_ENDPOINT:
